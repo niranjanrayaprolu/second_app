a1ab046677d5df0e1de4ca77ba778b46e9631d35
Slashroot <slashroot@slashroot-ThinkCentre-M70e.(none)>
2012-12-06 12:09:11 +0530

    added gem-execjs

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecondApp::Application.config.secret_token = '49ec9272948c4d0c7fd4cc9d8adb8128c163f23cd50575d053167bd8568a52097a89e8c9068e82b161cb5a4e24448eaceb8ebf4d8b63a91e2487ca786a1f10ab'
